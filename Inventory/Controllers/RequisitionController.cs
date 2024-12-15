2bd3da22c1a57802c33c652114e3d1cabfa3ae5f
PrasantaSaren <prasantasaren53@gmail.com>
2024-12-15 17:45:13 +0530

    nc

--- Inventory/Controllers/RequisitionController.cs
+++ Inventory/Controllers/RequisitionController.cs
@@ -18,17 +18,80 @@ namespace Inventory.Controllers
             _repo = repo;
             _config = config;
         }
+
+        
         [HttpGet]
-        [Route("GetArea")]
-        public IActionResult GetArea(long unitId, long companyId)
+        [Route("GetRequisitionUnitDetails")]
+        public IActionResult GetRequisitionUnitDetails(string type, long userID)
         {
-            var result = _repo!.GetArea(unitId, companyId);
+            var result = _repo!.GetRequisitionUnitDetails(type, userID);
+            if ((result.UnitDetailsList!.Count == 0))
+            {
+                return NotFound("No data found for the provided UnitID and CompanyID.");
+            }
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("GetRequisitionUnitAreaDetails")]
+        public IActionResult GetRequisitionUnitAreaDetails(long unitId, long companyId)
+        {
+            var result = _repo!.GetRequisitionUnitAreaDetails(unitId, companyId);
             if ((result.Areas!.Count == 0) && (result.UnitInfo == null))
             {
                 return NotFound("No data found for the provided UnitID and CompanyID.");
             }
             return Ok(result);
         }
+
+
+        [HttpGet]
+        [Route("GetRequisitionUnitAreaLocationDetails")]
+        public async Task<IActionResult> GetRequisitionUnitAreaLocationDetails(long unitId = 0, long areaId = 0)
+        {
+            var result = await _repo!.GetRequisitionUnitAreaLocationDetails(unitId, areaId);
+            if (result.Tables[0].Rows.Count <= 0)
+            {
+                return Ok(new { message = "No records found.", data = new List<Dictionary<string, object>>() });
+            }
+            var output = GeneralUtilityService.ConvertDataTableToDictionaryList(result.Tables[0]);
+            return Ok(output);
+        }
+
+        [HttpPost("InsertRequisitionDetails")]
+        public async Task<IActionResult> InsertRequisitionDetails([FromBody] Ims_Requisition_Model _bodyParams)
+        {
+            try
+            {
+                var result = await _repo!.InsertRequisitionDetails(_bodyParams);
+                if (result > 0)
+                {
+                    return Ok(new { Id = result, Message = "Requisition Details saved successfully." });
+                }
+                else
+                {
+                    return StatusCode(500, "An error occurred while saving the Requisition Details.");
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                return StatusCode(500, new { message = "A database error occurred.", error = sqlEx.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+
+
+
+
+
+
+
+
+
         [HttpPost("InsertOrUpdateArea")]
         public IActionResult InsertOrUpdateArea([FromBody] Ims_Requisition_AreaRequest _bodyParams)
         {
@@ -53,18 +116,7 @@ namespace Inventory.Controllers
                 return StatusCode(500, $"Internal server error: {ex.Message}");
             }
         }
-        [HttpGet]
-        [Route("GetUnitAreaLocation")]
-        public async Task<IActionResult> GetUnitAreaLocation(long unitId = 0, long areaId = 0)
-        {
-            var result = await _repo!.GetUnitAreaLocation(unitId, areaId);
-            if (result.Tables[0].Rows.Count <= 0)
-            {
-                return Ok(new { message = "No records found.", data = new List<Dictionary<string, object>>() });
-            }
-            var output = GeneralUtilityService.ConvertDataTableToDictionaryList(result.Tables[0]);
-            return Ok(output);
-        }
+        
         [HttpGet]
         [Route("GetRemarks")]
         public async Task<IActionResult> GetRemarks(long reqId = 0)
@@ -180,5 +232,32 @@ namespace Inventory.Controllers
             }
         }
         #endregion
+
+        
+
+        [HttpPost("UpdateRequisitionDetails")]
+        public async Task<IActionResult> UpdateRequisitionDetails([FromBody] Ims_Requisition_Model _bodyParams)
+        {
+            try
+            {
+                var result = await _repo!.UpdateRequisitionDetails(_bodyParams);
+                if (result > 0)
+                {
+                    return Ok(new { Id = result, Message = "Requisition details updated successfully." });
+                }
+                else
+                {
+                    return StatusCode(500, "An error occurred while updating the Requisition Details.");
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                return StatusCode(500, new { message = "A database error occurred.", error = sqlEx.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
     }
 }
