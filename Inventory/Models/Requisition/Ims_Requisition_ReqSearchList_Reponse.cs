8940a816eb1e2cd4ea36eccbdfb07fc4882c9c39
PrasantaSaren <prasantasaren53@gmail.com>
2024-12-15 21:34:45 +0530

    'nc'

--- Inventory/Models/Requisition/Ims_Requisition_ReqSearchList_Reponse.cs
+++ Inventory/Models/Requisition/Ims_Requisition_ReqSearchList_Reponse.cs
@@ -1,25 +1,32 @@
 namespace Inventory.Models.Requisition;
+//public class Ims_Requisition_ReqSearchList_Reponse
+//{
+//    public DateTime? StartDate = null;
+//    public DateTime? EndDate = null;
+//    public long? UnitId { get; set; } = 0;
+//    public string? ApprovalStatus { get; set; } = "";
+//    public string? ReqNo { get; set; } = "";
+//    public long? CompanyId { get; set; } = 0;
+//    public string? ReqType { get; set; } = "";
+//    public string? SearchStat { get; set; } = "";
+//    public long? ReqId { get; set; } = 0;
+//    public string? SBUType { get; set; } = "";
+//    public int? Layer { get; set; } = 0;
+//    public long? CreatedUID { get; set; } = 0;
+//}
+
 public class Ims_Requisition_ReqSearchList_Reponse
 {
-        private DateTime? _startDate;
-        private DateTime? _endDate;
-        public DateTime? StartDate
-        {
-            get => _startDate;
-            set => _startDate = string.IsNullOrWhiteSpace(value?.ToString()) ? null : value;
-        }
-        public DateTime? EndDate
-        {
-            get => _endDate;
-            set => _endDate = string.IsNullOrWhiteSpace(value?.ToString()) ? null : value;
-        }
-    public long? UnitId { get; set; } = 0;
-    public string? ApprovalStatus { get; set; } = "";
-    public string? ReqNo { get; set; } = "";
-    public long? CompanyId { get; set; } = 0;
-    public string? ReqType { get; set; } = "";
-    public string? SearchStat { get; set; } = "";
-    public long? ReqId { get; set; } = 0;
-    public string? SBUType { get; set; } = "";
-    public int? Layer { get; set; } = 0;
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public long? UnitId { get; set; }
+    public string? ApprovalStatus { get; set; }
+    public string? ReqNo { get; set; }
+    public long? CompanyId { get; set; }
+    public string? ReqType { get; set; }
+    public string? SearchStat { get; set; }
+    public long? ReqId { get; set; }
+    public string? SBUType { get; set; }
+    public int? Layer { get; set; }
+    public long? CreatedUID { get; set; }
 }
